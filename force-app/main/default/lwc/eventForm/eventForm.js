e7f72cf5215ee6fee73c20aac1ec67e4ca40db17
Mirosław Karwowski <miroslaw.karwowski@cloobees.com>
2022-07-27 13:51:47 +0200

    feat: add event form component which allow us to create new event

---
+++ force-app/main/default/lwc/eventForm/eventForm.js
@@ -0,0 +1,73 @@
+import { LightningElement, api } from 'lwc';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import EVENT_OBJECT from '@salesforce/schema/Event__c';
+import EVENT_NAME from '@salesforce/schema/Event__c.Name__c';
+import EVENT_ORGANIZER from '@salesforce/schema/Event__c.Organizer__c';
+import EVENT_START_DATE from '@salesforce/schema/Event__c.Start_Date_Time__c';
+import EVENT_END_DATE from '@salesforce/schema/Event__c.End_Date_Time__c';
+import EVENT_MAX_ATTENDEES from '@salesforce/schema/Event__c.Max_Seats__c';
+import EVENT_DETAIL from '@salesforce/schema/Event__c.Event_Detail__c';
+import EVENT_LOCATION from '@salesforce/schema/Event__c.Location__c'
+
+export default class EventForm extends LightningElement {
+    eventObject = EVENT_OBJECT;
+    eventOrganizer = EVENT_ORGANIZER;
+    eventLocation = EVENT_LOCATION;
+
+    formFields = {
+        Name:'',
+        Organizer:'',
+        Start_Date: '',
+        End_Date: '',
+        Max_Attendees: '',
+        Location: '',
+        Detail:''
+    }
+    changeHandler(event){
+        const{value, name} = event.target
+        this.formFields = { ...this.formFields, [name]:value}
+    }
+    changeHandlerOrganizer(event) {
+        if(!event.preventDefault){
+            event.preventDefault();
+        }
+        this.formFields.Organizer = event.target.value;
+    }
+    changeHandlerLocation(event){
+        if(!event.preventDefault){
+            event.preventDefault();
+        }
+        this.formFields.Location = event.target.value;
+    }
+    handleSave(){
+        const fields = {}
+        fields[EVENT_NAME.fieldApiName] = this.formFields.Name
+        fields[EVENT_ORGANIZER.fieldApiName] = this.formFields.Organizer
+        fields[EVENT_START_DATE.fieldApiName] = this.formFields.Start_Date
+        fields[EVENT_END_DATE.fieldApiName] = this.formFields.End_Date
+        fields[EVENT_MAX_ATTENDEES.fieldApiName] = this.formFields.Max_Attendees
+        fields[EVENT_LOCATION.fieldApiName] = this.formFields.Location
+        fields[EVENT_DETAIL.fieldApiName] = this.formFields.Detail
+        let recordInput = {apiName: EVENT_OBJECT.objectApiName, fields}
+        createRecord(recordInput).then(event =>{
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Event created',
+                    variant: 'success',
+                }),
+            );
+        }).catch(error => {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error creating record',
+                    message: error.body.message,
+                    variant: 'error',
+                }),
+            )
+        });
+    }
+
+}
