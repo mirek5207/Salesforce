a670dfa4a52b8c99b33cf2f5592453a00489c521
Mirosław Karwowski <miroslaw.karwowski@cloobees.com>
2022-07-28 13:04:27 +0200

    Remove Id from speaker detail data table

--- force-app/main/default/lwc/eventSpeakers/eventSpeakers.js
+++ force-app/main/default/lwc/eventSpeakers/eventSpeakers.js
@@ -3,21 +3,19 @@ import speakerData from '@salesforce/apex/EventSpeakerController.getEventSpeaker
 //Parent(Account) to child(Contact & Account)
 //Parent(Event) to Child(Event_Speaker & Speaker)
 const column = [
-    {label: 'Speaker id', fieldName : 'Id'},
     {label: 'Speaker name', fieldName : 'Name'},
     {label: 'Speaker Phone', fieldName : 'Phone__c'},
     {label: 'Speaker Email', fieldName : 'Email__c'},
 ]
 export default class EventSpeakers extends LightningElement {
-    @api recordId;
     speakerArray= [];
     column1 = column
-    response;
-    
+    @api recordId;
+
+    @wire(speakerData)
     connectedCallback(){
         speakerData({eventId : this.recordId}).then(res=>{
             this.speakerArray = res;
-            console.log(this.speakerArray)
         }).catch(error=>{
             console.error(JSON.stringify(error))
         })
