8dc438e8c1b53cb86ef7ab7bd605c41acdafadb0
Mirosław Karwowski <miroslaw.karwowski@cloobees.com>
2022-07-29 09:57:30 +0200

    feat: add lwc component to display location details on Event record page

---
+++ force-app/main/default/lwc/eventLocation/eventLocation.js
@@ -0,0 +1,25 @@
+import { LightningElement,api,wire} from 'lwc';
+
+import locationData from '@salesforce/apex/EventLocationController.getLocationDataByEventId'
+
+export default class EventLocation extends LightningElement {
+    @api recordId;
+    locationArray = [];
+    locationIsEmpty = false;
+    @wire(locationData)
+    connectedCallback(){
+        locationData({eventId : this.recordId}).then(res=>{
+            console.log('Response:',JSON.stringify(res))
+            if(JSON.stringify(res) == '[]' || JSON.stringify(res) == '[{}]'){
+                this.locationIsEmpty = true;
+            }
+            else{
+                this.locationIsEmpty = false;
+                this.locationArray = res;
+            }
+        }).catch(error=>{
+            console.error(JSON.stringify(error))
+        })
+    }
+
+}
